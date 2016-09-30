29727c10bc72229c5a75b9b184ca79f1a6a8845a
Alex <aacosta@eafit.edu.co>
2016-09-30 16:27:28 -0500

    Registro sin terminar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,50 +8,55 @@
  *
  * Main module of the application.
  */
-angular
-  .module('pi1App', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
+ angular
+ .module('pi1App', [
+  'ngAnimate',
+  'ngAria',
+  'ngCookies',
+  'ngMessages',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/sobreNosotros', {
-        templateUrl: 'views/sobreNosotros.html',
-        controller: 'SobreNosotrosCtrl',
-        controllerAs: 'sobreNosotros'
-      })
-      .when('/contact', {
-        templateUrl: 'views/contact.html',
-        controller: 'ContactCtrl',
-        controllerAs: 'contact'
-      })
-      .when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl',
-        controllerAs: 'login'
-      })
-      .when('/testimonios', {
-        templateUrl: 'views/testimonios.html',
-        controller: 'TestimoniosCtrl',
-        controllerAs: 'testimonios'
-      })
-       .when('/noticias', {
-        templateUrl: 'views/noticias.html',
-        controller: 'NoticiasCtrl',
-        controllerAs: 'noticias'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+ .config(function ($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'views/main.html',
+    controller: 'MainCtrl',
+    controllerAs: 'main'
+  })
+  .when('/sobreNosotros', {
+    templateUrl: 'views/sobreNosotros.html',
+    controller: 'SobreNosotrosCtrl',
+    controllerAs: 'sobreNosotros'
+  })
+  .when('/contact', {
+    templateUrl: 'views/contact.html',
+    controller: 'ContactCtrl',
+    controllerAs: 'contact'
+  })
+  .when('/login', {
+    templateUrl: 'views/login.html',
+    controller: 'LoginCtrl',
+    controllerAs: 'login'
+  })
+  .when('/testimonios', {
+    templateUrl: 'views/testimonios.html',
+    controller: 'TestimoniosCtrl',
+    controllerAs: 'testimonios'
+  })
+  .when('/noticias', {
+    templateUrl: 'views/noticias.html',
+    controller: 'NoticiasCtrl',
+    controllerAs: 'noticias'
+  })
+  .when('/registro', {
+    templateUrl: 'views/registro.html',
+    controller: 'RegistroCtrl',
+    controllerAs: 'registro'
+  })
+  .otherwise({
+    redirectTo: '/'
   });
+});
